ab93f9f49544de13c4a8447e7ed6cbce60e26378
Rendy Rianda <37236698+rendyrianda@users.noreply.github.com>
2021-04-14 18:18:14 +0700

    bot.py
    bot

---
+++ bot.py
@@ -0,0 +1,36 @@
+# bot.py
+import os
+import random
+import discord
+
+TOKEN = 'ODMxNzg1MTY1OTE0MjQzMDkz.YHaSTQ._jeei-czYhkUvgaaPkyHF9lFFZg'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+        print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    isi = [
+            'hmmm apa yaaa',
+            'enak aja minta flag',
+            'kayaknya ini : https://i.imgur.com/6AnLddq.png',
+            'bentar ingat-ingat dulu....',
+            'mungkin ini : QnVrYW4gZmxhZ255YSBIQUhBSEFIQUhBSEFIQQo=',
+            '```help : help\nabout : tentang bot\nFlag : ????```',
+            '```Version : 1.0.3\nAuthor : Rendy Rianda```',
+            ]
+    if message.content == 'flag':
+        response = isi[random.randint(0,4)]
+        await message.channel.send(response)
+    if message.content == 'help':
+        response = isi[5]
+        await message.channel.send(response)
+    if message.content == 'about':
+        response = isi[6]
+        await message.channel.send(response)
+client.run(TOKEN)
