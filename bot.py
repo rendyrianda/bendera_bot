a5faec97a1a8e40bc555175cf3a2af478477cf04
Rendy Rianda <37236698+rendyrianda@users.noreply.github.com>
2021-04-15 17:03:08 +0700

    1.0.1

--- bot.py
+++ bot.py
@@ -16,13 +16,15 @@ async def on_message(message):
     if message.author == client.user:
         return
     isi = [
-            'hmmm apa yaaa',
-            'enak aja minta flag',
-            'kayaknya ini : https://i.imgur.com/6AnLddq.png',
-            'bentar ingat-ingat dulu....',
-            'mungkin ini : QnVrYW4gZmxhZ255YSBIQUhBSEFIQUhBSEFIQQo=',
-            '```help : help\nabout : tentang bot\nFlag : ????```',
+            'i hear something about this : `https://i.imgur.com/HEZ0ziM.png`',
+            'not sure but i saw this \nXKZ{jCs4V_1v1_nt4oVg4}',
+            'yesterday i saw this on street : https://i.imgur.com/6AnLddq.png',
+            '`probably this one, https://bit.ly/IqT6zt`',
+            'there is strange voice telling me bout this sequence of character \nQnVrYW4gZmxhZ255YSBIQUhBSEFIQUhBSEFIQQo=',
+            '```help : help\nabout : tentang bot\nflag : ????```',
             '```Version : 1.0.3\nAuthor : Rendy Rianda\nRepo Github : https://github.com/rendyrianda/bendera_bot```',
+            'yes can you give me a fish??? i want a fish!',
+            'meow','meowww','meowwww','meow meow',
             ]
     if message.content == 'flag':
         response = isi[random.randint(0,4)]
@@ -32,5 +34,11 @@ async def on_message(message):
         await message.channel.send(response)
     if message.content == 'about':
         response = isi[6]
+        await message.channel.send(response)
+    if message.content == 'are you hungry?':
+        response = isi[7]
+        await message.channel.send(response)
+    else : #probably bad idea to put this but it's work fine
+        response = isi[random.randint(8,11)]
         await message.channel.send(response)
 client.run(TOKEN)
